86cd782da7adfde89b4fd75884068ba6e9f58e29
Riya <riyanegi221b@gmail.com>
2020-12-30 12:34:34 +0530

    Add chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
+import React, { PureComponent } from "react"
 import './App.css';
+import ChartGraph from "./ChartGraph"
+class App extends PureComponent {
+  state = {
+    isloading: true,
+    data: []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      Stock Market Timeline
-    </div>
-  );
+  componentDidMount() {
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo')
+      .then((resp) => resp.json())
+      .then(json => {
+        this.setState({ isloading: false, data: json })
+      })
+      .catch((err) => console.log("error->", err))
+  }
+  render() {
+    const { isloading, data } = this.state;
+    delete data['Meta Data']
+    let objArray = [];
+    let graphArray = [];
+    let newData = Object.values(data)[0]
+    if (newData) {
+      Object.keys(newData).forEach(key => objArray.push({
+        name: key,
+        ...Object.values(newData[key])
+      }));
+      for (let i = 0; i < objArray.length; i += 14) {
+        graphArray.push(objArray[i])
+      }
+    }
+
+
+    if (isloading) {
+      return <div style={{ color: "white" }}>
+        Loading data....
+      </div>
+    }
+
+    return (
+      <div className="App" >
+        Stock Market Timeline
+        <ChartGraph graphData={graphArray} />
+        <Report data={graphArray}
+          filename={filename}
+          fields={fields}
+          style={style}
+          text={text} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/ChartGraph.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+
+export default class ChartGraph extends PureComponent {
+    render() {
+        console.log("data->", this.props.graphData)
+
+        return (
+            <LineChart
+                width={800}
+                height={400}
+                data={this.props.graphData}
+                margin={{
+                    top: 5, right: 10, left: 10, bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey={0} stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey={1} stroke="#82ca9d" />
+                <Line type="monotone" dataKey={2} stroke="#00FFFF" />
+                <Line type="monotone" dataKey={3} stroke="#F2EA02" />
+            </LineChart>
+        );
+    }
+}
